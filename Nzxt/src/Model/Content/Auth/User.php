dc42d95e8e811b4a6afb23949f841b5b34ae283a
Sven Erens <sven.erens@mirumagency.com>
2017-01-04 11:59:12 +0100

    ADDED overridden method to assign new hashed password to user-record

--- Nzxt/src/Model/Content/Auth/User.php
+++ Nzxt/src/Model/Content/Auth/User.php
@@ -2,6 +2,7 @@
 namespace Nzxt\Model\Content\Auth;
 
 use Nzxt\Model\Content\AbstractContent;
+use Signature\Persistence\ActiveRecord\RecordInterface;
 
 /**
  * Class User
@@ -12,20 +13,17 @@ class User extends AbstractContent
     protected $icon = 'fa fa-user-o';
 
     /**
-     * @return string
+     * Description of the field elements.
+     * @var array
      */
-    public function getUsername(): string
-    {
-        return $this->getFieldValue('username');
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword(): string
-    {
-        return $this->getFieldValue('password');
-    }
+    protected $fieldDescription = [
+        'username' => [
+            'elementClassname' => \Signature\Html\Form\Element\Input::class,
+        ],
+        'password' => [
+            'elementClassname' => \Signature\Html\Form\Element\Password::class,
+        ],
+    ];
 
     /**
      * @return string
@@ -33,5 +31,20 @@ class User extends AbstractContent
     public function getFullname(): string
     {
         return $this->getNode()->getTitle();
+    }
+
+    /**
+     * When the password has changed since it was loaded from the db, it has to be hashed again.
+     * @param string $field
+     * @param mixed $value
+     * @return RecordInterface
+     */
+    public function setFieldValue(string $field, $value): RecordInterface
+    {
+        if ('password' == $field && $this->hasField('password') && $value !== $this->getFieldValue('password')) {
+            $value = password_hash($value, PASSWORD_DEFAULT);
+        }
+
+        return parent::setFieldValue($field, $value);
     }
 }
